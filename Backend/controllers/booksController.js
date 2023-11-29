19fd3a106f422f1a55fb4cbc185d7b9a30892348
Kipkorir74 <grahamhim74@gmail.com>
2023-11-29 14:32:07 +0300

    cleaned my apis

--- Backend/controllers/booksController.js
+++ Backend/controllers/booksController.js
@@ -9,27 +9,36 @@ const mongoose = require('mongoose')
 
 const getAllBooks = async (req, res) => {
     // const user_id =req.user._id
-    // const workouts = await workoutModel.find({user_id}).sort({ createdAt: -1 })
-    // res.status(200).json(workouts)
+    const books = await booksModel.find({})
+    res.status(200).json({
+        count: books.length,
+        data: books
+    })
 
 }
 
 //Retrieve a single workout
 const getBook = async (req, res) => {
-    // const { id } = req.params
+    try {
+        const { id } = req.params
+        const book = await booksModel.findById(id)
+
+        return res.status(200).json(book)
+
+    } catch (error) {
+
+        res.status(404).json({ error:"No such book exists" })
+
+    }
+
 
     // if (!mongoose.Types.ObjectId.isValid(id)) {
-    //     return res.status(404).json({ error: 'No such workout exists' })
+    //     return res.status(404).json({ error: 'No such book exists' })
     // }
 
-    // const workout = await workoutModel.findById(id)
 
-    // if (!workout) {
-    //     return res.status(404).json({ error: "No such workout exists" })
-    // }
-    // else {
-    //     return res.status(200).json(workout)
-    // }
+
+
 }
 
 //Create a single workout
@@ -39,31 +48,31 @@ const createBook = async (req, res) => {
 
     let emptyFields = []
 
-    if(!title){
+    if (!title) {
         emptyFields.push('title')
     }
 
-    if(!author){
+    if (!author) {
         emptyFields.push('author')
     }
 
-    if(!publishYear){
+    if (!publishYear) {
         emptyFields.push('publishYear')
     }
 
-    if(emptyFields.length > 0) {
+    if (emptyFields.length > 0) {
         return res.status(400).json({
-            error:"Please fill all the fields", emptyFields
+            error: "Please fill all the fields", emptyFields
         })
     }
 
     try {
         // const user_id = req.user._id
         // Create a new workout document using the Mongoose model booksModel.
-        const book = await booksModel.create({ title, author, publishYear})
+        const book = await booksModel.create({ title, author, publishYear })
 
         // Respond with a 200 (OK) status code and the created workout as JSON.
-        res.status(200).json(book)
+        res.status(200).send({message:"Book created successfully"})
     }
     catch (error) {
         // If there is an error, respond with a 400 (Bad Request) status code and an error message as JSON.
@@ -73,38 +82,48 @@ const createBook = async (req, res) => {
 
 //Delete a workout
 const deleteBook = async (req, res) => {
-    // const { id } = req.params
+
+    try{
+        const { id } = req.params
+
+        const book = await booksModel.findOneAndDelete({ _id: id })
+
+        res.status(200).send({message:"Book deleted successfully"})
+
+    }
+    catch(error){
+        res.status(404).json({ error:"No such book exists" })
+    }
+
 
     // if (!mongoose.Types.ObjectId.isValid(id)) {
     //     return res.status(404).json({ error: 'No such workout exists' })
     // }
-
-    // const workout = await workoutModel.findOneAndDelete({ _id: id })
-
-    // if (!workout) {
-    //     return res.status(404).json({ error: "No such workout exists" })
-    // }
-
-    // res.status(200).json(workout)
 }
 
 // Update a workout
 const updateBook = async (req, res) => {
-    // const { id } = req.params
+    try {
+        const { id } = req.params
 
     // if (!mongoose.Types.ObjectId.isValid(id)) {
     //     return res.status(404).json({ error: 'No such workout exists' })
     // }
 
-    // const workout = await workoutModel.findByIdAndUpdate({_id:id}, {
-    //     ...req.body
-    // })
+    const book = await booksModel.findByIdAndUpdate({_id:id}, {
+        ...req.body
+    })
 
-    // if (!workout) {
-    //     return res.status(404).json({ error: "No such workout exists" })
-    // }
+    if (!book) {
+        return res.status(404).json({ error: "No such book exists" })
+    }
 
-    // res.status(200).json(workout)
+    res.status(200).send({message:"Book updated successfully"})
+    }
+    catch(error){
+        res.status(404).json({ error:"No such book exists" })
+    }
+    
 }
 
 module.exports = {
