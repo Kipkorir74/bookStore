956a35490d2a78c3efa293c0b9861b817a220b15
Kipkorir74 <grahamhim74@gmail.com>
2023-11-30 13:09:46 +0300

    delete file

--- Frontend/src/pages/deleteFile.jsx
+++ Frontend/src/pages/deleteFile.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+import Spinner from '../components/Spinner';
+import BackButton from '../components/Backbutton';
 
 const deleteFile = () => {
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const handleDelete = () => {
+    setLoading(true)
+
+    axios
+      .delete(`http://localhost:4000/api/bookstore/${id}`)
+      .then(() => {
+        setLoading(true);
+        alert('Book deleted Succefully')
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error occured while deleting the book');
+        console.log(error)
+      })
+  }
+
   return (
-    <div class="bg-slate-400">
-      delete file
+    <div class="p-4">
+      <h1 className='text-3xl my-4'>Delete Book</h1>
+      {
+        loading ? <Spinner /> : ''
+
+      }
+      <div className='flex flex-col border-2 border-sky-700 rounded-xl w-[600px] p-8 mx-auto'>
+
+        <h3 className='text-2xl'>Confirm if you want to delete the Book</h3>
+        <button className='p-4 bg-red-700 text-white m-8 w-full' onClick={handleDelete}>
+          Delete
+        </button>
+      </div>
     </div>
   )
 }
